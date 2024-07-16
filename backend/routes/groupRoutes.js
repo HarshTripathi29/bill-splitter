1e6184ae042b9631bd173aba6a66f52c6f317db6
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-07-16 12:03:51 +0530

    added expenses

--- backend/routes/groupRoutes.js
+++ backend/routes/groupRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { createGroup } = require('../controllers/groupController');
+const { createGroup, getAllGroups, addExpense, settleUp, getGroupDetails } = require('../controllers/groupController');
 
 const router = express.Router();
 
 router.post('/create', createGroup);
+router.get('/', getAllGroups);
+router.post('/:groupId/addExpense', addExpense);
+router.post('/:groupId/settleUp', settleUp);
+router.get('/:id', getGroupDetails);
 
 module.exports = router;
