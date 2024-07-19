f63bda28b4a0160262a0bd4879a4e4cc95dfb459
Harsh Tripathi <106900613+HarshTripathi29@users.noreply.github.com>
2024-07-19 11:32:47 +0530

    update expense model

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const expenseSchema = new mongoose.Schema({
-    groupId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Group',
-        required: true,
-    },
-    category: String,
-    totalAmount: Number,
-    paidBy: String,
-    date: {
-        type: Date,
-        default: Date.now,
-    },
+const expenseSchema = new Schema({
+    totalAmount: { type: Number },
+    category: { type: String },
+    paidBy: { type: String},
+    date: { type: Date, default: Date.now },
+    groupId: { type: Schema.Types.ObjectId, ref: 'Group'},
+    perPersonAmount: { type: Number},
+    owes: [{
+        userId: { type: Schema.Types.ObjectId, ref: 'User' },
+        amount: { type: Number }
+    }],
+    owedTo: [{
+        userId: { type: Schema.Types.ObjectId, ref: 'User' },
+        amount: { type: Number}
+    }]
 });
 
-module.exports = mongoose.model('Expense', expenseSchema);
+const Expense = mongoose.model('Expense', expenseSchema);
+module.exports = Expense;
